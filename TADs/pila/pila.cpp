b5572b4d9a2300ac5c7f52ac5421b1d798008073
Juan <104105383+jmkechichian@users.noreply.github.com>
2023-08-06 18:21:24 -0300

    Update pila.cpp

--- TADs/pila/pila.cpp
+++ TADs/pila/pila.cpp
@@ -17,3 +17,45 @@ Pila crearPila(int cota) {
     p ->cota = cota;
     return p;
 }
+
+void destruirPila(Pila p) {
+    delete[] p->array;
+    delete p;
+}
+
+bool esVaciaPila(Pila p) {
+    return (p->tope == -1);
+}
+
+bool esLlenaPila(Pila p){
+     return (p->tope == p->cota - 1);
+}
+
+void apilar(Pila &p, int i){
+    if (!esLlenaPila(p)) {
+        p->tope++;
+        p->array[p->tope] = i;
+    }
+}
+
+int cima(Pila p) {
+    if (!esVaciaPila(p)) {
+        return p->array[p->tope];
+    }
+    // Manejo del error o retorno especial en caso de pila vacía
+    else {
+      return -1; // Por ejemplo, en este caso, -1 podría indicar pila vacía
+    }
+}
+
+int desapilar(Pila p) {
+    if (!esVaciaPila(p)) {
+        int dato = p->array[p->tope];
+        p->tope--;
+        return dato;
+    }
+    // Manejo del error o retorno especial en caso de pila vacía
+    else {
+      return -1; // Por ejemplo, en este caso, -1 podría indicar pila vacía
+  }
+}
